fda9af633bcf78fbe9091aa07a82d77b4e2c4d84
noah <22294@ecam.be>
2025-03-27 11:25:44 +0100

    a

--- exo_3.py
+++ exo_3.py
@@ -1,13 +1,13 @@
 from time import sleep 
 def delay(t):
-    def decorator(f):
-        def wrapper(*args,**kwargs):
+    def decorator(f):  # le decorateur renvoie la fonction mais modifié , ajout d'un comportement avant et  près l'appel d'une fonction
+        def wrapper(*args,**kwargs): # génerateur lui renvoie dez décorateurs
             sleep(t)
             return f(*args,**kwargs)
         return wrapper
     return decorator
 
-@delay(2)
+@delay(2) # le @ syntaxe qui indique que y'aura un décorateur
 def printnum(i):
     print(i)
 
@@ -16,6 +16,6 @@ def binrep(n):
     while n > 0:
         yield n % 2
         n//=2
-for b in binrep(12):
+for b in binrep(334):
     print(b)
  
--- test.labo3/exo5.py
+++ test.labo3/exo5.py
@@ -3,11 +3,26 @@ def decorate(f):
         print('Avant appel')
         f()
         print('Après appel')
-        
     return wrapper
  
+@decorate
 def sayhello():
-  print('Hello!')
+    print('Hello!')
  
-g = decorate(sayhello)
-g()
+sayhello()
+
+class Natural:
+    def __init__(self,n):
+        self.__n = n 
+        self.__bin_n = n 
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.__bin_n==0:
+            raise StopIteration()
+        bit = self.__bin_n % 2
+        self.__bin_n //=2
+        return bit 
+for b in Natural(12):
+    print(b)
