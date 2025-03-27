f07a4a1fbdd9ea5736c3c5c3a2d44be1bbb6348b
noah <22294@ecam.be>
2025-03-27 09:10:46 +0100

    done

--- exo_1.py
+++ exo_1.py
@@ -1,1 +1,22 @@
-a= 2
+
+def hello():
+    print("hello python ")
+
+def call(f,*args,**kwargs):
+    return f(*args,**kwargs)
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def compute(a,b,op=add):
+    return op(a,b)
+
+
+
+call(hello)
+print(call(add,40,2))
+print(call(sub,40,2))
+
+    
+
