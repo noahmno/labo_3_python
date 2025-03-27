5895d73508eda07173ee8cf701bb055dcee0ea6c
noah <22294@ecam.be>
2025-03-27 10:00:07 +0100

    p

--- exo_2.py
+++ exo_2.py
@@ -1,1 +1,18 @@
-print("hello")
+from time import sleep
+
+def delay(f):
+    def wrapper(*args,**kwargs):
+        sleep(1)
+        return f(*args,**kwargs)
+    return wrapper 
+
+
+@delay
+def printnum(i):
+    print(i)
+
+cnt= 3
+while cnt> 0 :
+    printnum(cnt)
+    cnt -= 1
+print('KA-BOOM!')
